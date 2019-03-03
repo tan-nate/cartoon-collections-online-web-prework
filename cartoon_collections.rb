10ef83e66d82a61ef48caa141a0eae3400bffad0
Nate Tan <nktan93@gmail.com>
2019-03-03 06:08:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(words)
-  words.any? do word
+  words.any? do |word|
+    
 end
 
 def find_the_cheese# code an argument here
